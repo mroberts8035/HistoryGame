e93656305fd37aac9724e83c5a91ec08077e04d1
Matthew Roberts <mroberts8035@outlook.com>
2022-02-22 21:11:46 -0600

    Added TrainingDummy Class

---
+++ Enemies/TrainingDummy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HistoryLibrary;
+
+namespace Enemies
+{
+    public class TrainingDummy : Opponent
+    {
+        public TrainingDummy()
+        {
+            MaxLife = 5;
+            MaxDamage = 1;
+            Name = "Training Dummy";
+            Life = 5;
+            HitChance = 15;
+            Block = 5;
+            MinDamage = 1;
+            Description = "A standard training dummy. Designed to practice on without fear!";
+        }
+    }
+}
