7bf4bda3ceaae7ad54fe6b0326440b0c39fe2f4b
Matthew Roberts <mroberts8035@outlook.com>
2022-02-22 21:06:55 -0600

    Added Weapon Class

---
+++ HistoryLibrary/Weapon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistoryLibrary
+{
+    public class Weapon
+    {
+        //FIELDS
+        private int _minDamage;
+
+        //PROPERTIES
+        public int MaxDamage { get; set; }
+
+        public string Name { get; set; }
+
+        public int BonusHitChance { get; set; }
+
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                if (value > 0 && value <= MaxDamage)
+                {
+                    _minDamage = value;
+                }
+                else
+                {
+                    _minDamage = 1;
+                }
+            }
+        }
+
+        //CONSTRUCTORS
+        public Weapon(int minDamage, int maxDamage, string name, int bonusHitChance)
+        {
+            MaxDamage = maxDamage;
+            MinDamage = minDamage;
+            Name = name;
+            BonusHitChance = bonusHitChance;
+        }
+
+        public Weapon() { }
+
+
+        //METHODS
+
+        public override string ToString()
+        {
+            //return base.ToString(); 
+            return string.Format("{0}\nDamage : {1} to {2}\nBonus Hit: {3}", Name, MinDamage, MaxDamage, BonusHitChance);
+        }
+    }
+}
